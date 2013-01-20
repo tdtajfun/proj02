bc8755049b14bcfee4dbd453c91fd07b738b85a0
tomek <tomek@tomek.pl>
2013-01-20 08:10:50 +0100

    change sample submodule

---
+++ sample/pole/pole_prost.c
@@ -0,0 +1,6 @@
+#include "pole_prost.h"
+
+int f(int a, int b) 
+{
+ return a*b;
+}
---
+++ sample/sample.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "./pole/pole_prost.h"
+int main()
+{
+ printf("%d\n",f(2,4));
+ return 0;
+}
